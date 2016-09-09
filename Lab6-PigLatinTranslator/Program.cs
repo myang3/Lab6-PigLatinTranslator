c2cc1a780672bd51f8a35f33dd5f98c94113da42
Mo Yang <moyang9192@gmail.com>
2016-09-09 13:48:09 -0400

    Add files via upload

---
+++ Lab6-PigLatinTranslator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_PigLatinTranslator
+{
+    class Program
+    {
+        static string play;
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Welcome to the Pig Latin Translator!");
+                do
+                {
+                    Console.WriteLine("\nEnter a line to be translated:");
+                    string lineTranslate = Console.ReadLine();
+
+                    translator line = new translator();
+                    string newSentence = line.Translate(lineTranslate.ToLower());
+
+                    Console.WriteLine(newSentence);
+
+                    continueOrQuit();
+
+                } while (play.ToLower() == "y" || play.ToLower() == "yes");
+            }
+
+        private static void continueOrQuit()
+        {
+            Console.WriteLine("\nTranslate another line? (y/n)");
+            play = Console.ReadLine();
+        }
+    }
+}
+
---
+++ Lab6-PigLatinTranslator/Translator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_PigLatinTranslator
+{
+       public class translator
+    {
+        public string Translate(string wordToTranslate)
+
+        {
+
+        string firstcharacter = wordToTranslate.Substring(0, 1);
+        if (IsVowel(firstcharacter))
+        {
+        return wordToTranslate + "way";
+        }
+
+    int startPositionOfFirstVowel = 0;
+    for (int i = 0; i < wordToTranslate.Length; i++)
+        {
+        char[] letters = wordToTranslate.ToArray();
+        string testLetter = letters[i].ToString();
+    if(IsVowel(testLetter)){
+    startPositionOfFirstVowel = i;
+        break;
+        }
+}
+
+string firstconstanant = wordToTranslate.Substring(0, startPositionOfFirstVowel);
+string remainingword = wordToTranslate.Substring(startPositionOfFirstVowel);
+string translatedword = remainingword + firstconstanant + "ay";
+return translatedword;
+    }
+
+    public bool IsVowel(string testcharacter)
+{
+    if (testcharacter == "a"||testcharacter == "e"||testcharacter == "i"||testcharacter == "o"  || testcharacter == "u" || testcharacter == "A" || testcharacter == "E" || testcharacter == "I" || testcharacter == "O" || testcharacter == "U") { return true; }
+    else return false;
+}
+
+    }
+}
+
+/*
+  string a_word=”help!”;
+			0,1,2,3
+String [] wordAsArray= a _word.ToArray() //changes it into characters array by letter
+For (int i=0; i<wordAsArray.Length;i++)
+{string testLetter = wordAsArray[]; //way1
+String versiontest2 = a.word.substring(i,1); //way2
+}
+Check each character, use array or substring
+*/
+
